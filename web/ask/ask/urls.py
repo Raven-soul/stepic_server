955d029d3baa8f725101c1945dfee03350b59922
Crow <86617628+Raven-soul@users.noreply.github.com>
2023-03-01 12:10:58 +0300

    2.1.11 attempt 2

--- web/ask/ask/urls.py
+++ web/ask/ask/urls.py
@@ -1,30 +1,22 @@
-"""ask URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.conf.urls import patterns, include, url
 
-admin.autodiscover()
+from ask.views import found, not_found
+
+h = bytes("dfdf", encoding="utf8")
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'qa.views.test'),
-    url(r'^login/', 'qa.views.test'),
-    url(r'^signup', 'qa.views.test'),
-    url(r'^question/.+', 'qa.views.test'),
-    url(r'^ask/', 'qa.views.test'),
-    url(r'^popular', 'qa.views.test'),
-    url(r'^new', 'qa.views.test')
+
+    url(r'^$', found),
+
+    url(r'^login/', found),
+    url(r'^signup/', found),
+    url(r'^ask/', found),
+    url(r'^popular/', found),
+    url(r'^new/', found),
+
+    url(r'^admin/', admin.site.urls),
+    url(r'^question/', include('qa.urls')),
+
+    url(r'^', not_found),
 ]
